ec9687cad9196de119660ef07cbe99fa115bd40b
superadminbayantrack <superadminbayantrack@gmail.com>
2026-05-09 16:11:32 +0800

    netlify style

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,5 +1,6 @@
 import serverless from "serverless-http";
+import { createServer } from "../../server/index.ts";
 
-import { createServer } from "../../server";
+const app = createServer();
 
-export const handler = serverless(createServer());
+export const handler = serverless(app);
